56ac051420833dd0e4d40735d9f19afac9ada420
Daniel Steen Losvik <95danlos65@gmail.com>
2017-02-16 15:03:54 +0100

    plugins added

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "phonegap-nfc.NFC",
+        "file": "plugins/phonegap-nfc/www/phonegap-nfc.js",
+        "pluginId": "phonegap-nfc",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "phonegap-nfc": "0.6.6"
+};
+// BOTTOM OF METADATA
+});
